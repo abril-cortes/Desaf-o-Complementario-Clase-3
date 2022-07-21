2644497ebc138bffe14dacc9d1fdbcbd5ec49a2e
Abril Cortes <abicortes@hotmail.com.ar>
2022-07-21 15:59:39 -0300

    modificaciones

--- js/index.js
+++ js/index.js
@@ -2,21 +2,25 @@
 //Desafío con Ciclo For
 
 
-let numero = parseInt(prompt("Ingrese un nro:"));
+/*let numero = parseInt(prompt("Ingrese un nro:"));
 for (i = 0; i < numero; i++) {
   console.warn("Gracias por el numero!");
-}
+}*/
 
 //otro ejercicio con for
+
 /*let numeroIngresado = parseInt(prompt("Ingrese un numero del 1 al 10"));
 for(i = 0; i < numeroIngresado; i+=2) {
     console.log(i);
+} if (numeroIngresado > 10) {
+  alert("ERROR: El número debe estar entre 1 - 10!");
 }*/
 
 //Desafío con Ciclo While
-/*let password = prompt("Ingrese su contraseña");
+
+let password = prompt("Ingrese su contraseña:");
 while (password != "Micontraseña") {
     console.error(password + "<-- Esta contraseña es incorrecta!");
     password = prompt("Ingrese otra palabra:");
 }
-alert("Contraseña correcta!");*/
+alert("Contraseña correcta!");
