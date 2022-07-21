3c060071ab016cbbb4dc0d5a02caf22e7b1c77ff
Abril Cortes <abicortes@hotmail.com.ar>
2022-07-21 15:49:04 -0300

    Desafio complementario terminado

---
+++ js/index.js
@@ -0,0 +1,22 @@
+
+//Desafío con Ciclo For
+
+
+let numero = parseInt(prompt("Ingrese un nro:"));
+for (i = 0; i < numero; i++) {
+  console.warn("Gracias por el numero!");
+}
+
+//otro ejercicio con for
+/*let numeroIngresado = parseInt(prompt("Ingrese un numero del 1 al 10"));
+for(i = 0; i < numeroIngresado; i+=2) {
+    console.log(i);
+}*/
+
+//Desafío con Ciclo While
+/*let password = prompt("Ingrese su contraseña");
+while (password != "Micontraseña") {
+    console.error(password + "<-- Esta contraseña es incorrecta!");
+    password = prompt("Ingrese otra palabra:");
+}
+alert("Contraseña correcta!");*/
